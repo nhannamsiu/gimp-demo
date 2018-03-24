568c9168131495dc7bcf8386e53d2752432e1764
nhannamsiu <30719184+nhannamsiu@users.noreply.github.com>
2018-03-24 16:46:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# gimp-demo
+1. add C:\Program Files\GIMP 2\bin to system path
+2. copy example.py to C:\Program Files\GIMP 2\lib\gimp\2.0\plug-ins
+3. open cmd, test if gimp-console is found:
+	gimp-console-2.8.exe -v
+4. Paste this folder to where you want
+5. Open example.py in C:\Program Files\GIMP 2\lib\gimp\2.0\plug-ins to edit to your correct path:
+	inputName = r"C:\Users\User\Desktop\gimp-demo\a.jpg"
+	layerPath = r"C:\Users\User\Desktop\gimp-demo\temp"
+	outputPath = r"C:\Users\User\Desktop\gimp-demo\output"
+	
+6. open cmd:
+	gimp-console-2.8.exe -idf --batch-interpreter python-fu-eval -b "import example; example.run(2)" -b "pdb.gimp_quit(1)"
+
+7. Check the result in gimp-demo\output
---
+++ example.py
@@ -0,0 +1,42 @@
+from gimpfu import *
+
+#gimp-console-2.8.exe -idf --batch-interpreter python-fu-eval -b "import example; example.run(2)" -b "pdb.gimp_quit(1)"
+
+#call gimp.exe to execute the python script
+#python script must be written inside functions with at least 1 arg
+#the true format of image cant be changed by just rename the extension, like a.jpg -> a.png
+
+#for class members and method: https://www.gimp.org/docs/python/index.html
+
+def run(a):
+	inputName = r"C:\Users\User\Desktop\gimp-demo\a.jpg"
+	layerPath = r"C:\Users\User\Desktop\gimp-demo\temp"
+	outputPath = r"C:\Users\User\Desktop\gimp-demo\output"
+	
+	for i in range(1,a+1):
+		layerName = layerPath + "\\" + str(i) + ".jpg"
+		outputName = outputPath + "\\" + str(i) + ".jpg"
+		#load image to create project
+		image = pdb.file_jpeg_load(inputName, inputName)
+		#load layer to project and add to image
+		layer = pdb.gimp_file_load_layer(image, layerName)
+		image.add_layer(layer)
+	
+		#set layer opacity
+		pdb.gimp_layer_set_opacity(layer, 80)
+		
+		#transform perspective
+		#top-left, top-right, btm-left, btm-right
+		layer = pdb.gimp_item_transform_perspective(layer,82,55, 510,21,80,325, 510, 350)
+		
+		
+		#merge layer
+		tosave = pdb.gimp_image_merge_visible_layers(image, 0)
+		
+		#save
+		pdb.file_jpeg_save(image, tosave, outputName,outputName , "0.5",0,1,0,"",0,1,0,0)
+	
+	
+
+	
+	
